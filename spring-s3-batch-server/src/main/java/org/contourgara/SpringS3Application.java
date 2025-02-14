9c4f63ae8bca39516dd7144d9e83a933ba44e1f9
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-02-15 00:56:30 +0900

    返り値を正しく返すように修正

--- spring-s3-batch-server/src/main/java/org/contourgara/SpringS3Application.java
+++ spring-s3-batch-server/src/main/java/org/contourgara/SpringS3Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringS3Application {
     public static void main(String[] args) {
-        SpringApplication.run(SpringS3Application.class, args);
-        System.exit(0);
+        SpringApplication.exit(SpringApplication.run(SpringS3Application.class, args));
     }
 }
